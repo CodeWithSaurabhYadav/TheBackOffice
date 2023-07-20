8ea0d264d3399f9f64d7208fd377676a107a056f
Saurabh <sauraj.contact@gmail.com>
2023-07-20 18:12:46 +0530

    Fixed some bugs

--- app/middleware.py
+++ app/middleware.py
@@ -11,16 +11,3 @@ class CheckUserAuthenticationMiddleware:
 
         response = self.get_response(request)
         return response
-
-
-# class CheckUserAuthenticationMiddleware:
-#     def __init__(self, get_response):
-#         self.get_response = get_response
-
-#     def __call__(self, request):
-#         # Check if the user is authenticated and allowed to access the dashboard
-#         if not request.user.is_authenticated and request.path.startswith('/dashboard/'):
-#             return redirect('/login/?next=/dashboard/')
-
-#         response = self.get_response(request)
-#         return response
